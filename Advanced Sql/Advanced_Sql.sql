de890f33583644fdaf77da5b2ca5f65519bc4691
prebitha <81169091+prebitha@users.noreply.github.com>
2021-06-27 13:08:26 +0200

    Add files via upload

---
+++ Advanced Sql/Advanced_Sql.sql
@@ -0,0 +1,36 @@
+# 1.  An Sql query which returns columns with date, daily, weekly(rolling 7 day window) 
+# and monthly(rolling 30 day window) trading users for the last 30 days from current day.
+
+select date(execution_time) as date, 
+    
+    sum(day(execution_time)) over (
+        partition by  extract(week from execution_time)
+        order by day(execution_time)
+    ) as Weekly_Users,
+    sum(day(execution_time)) over (
+        partition by extract(day from execution_time)
+        order by day(execution_time)
+    ) as Daily_Users,
+        sum(day(execution_time)) over (
+        partition by extract(month from execution_time)
+        order by day(execution_time)
+    ) as Monthly_Users
+from trades
+group by date(execution_time);
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# 2. An query to indicate trading volume for each day ( sum of all trade total of that day).
+
+SELECT date(execution_time) as day ,sum(day(execution_time)) as total 
+from trades
+group by date(execution_time) ;
+
+#-------------------------------------------------------------------------------------------------------------------------------------
+
+# 3. A query that will return set of 30 rows for further anaylsis.
+
+SELECT date(execution_time) as day ,sum(day(execution_time)) as total 
+from trades
+group by date(execution_time)
+limit 30;
